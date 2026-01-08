3f95037cb7dfe1c9414d859d23aee625ffa9c56d
master <ted3131@diquest.com>
2026-01-08 10:12:57 +0900

    Initial commit

---
+++ src/main/java/com/example/view_shoppingmall/ViewShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.view_shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewShoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewShoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/view_shoppingmall/ViewShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.view_shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
